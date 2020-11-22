2fb084cf6260fe0ad5136b928ab978912304d609
clement piot <c.piot@me.com>
2020-11-22 12:14:06 +0100

    last step

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,16 +1,30 @@
 class DosesController < ApplicationController
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
   end
 
   def create
-  end
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
 
-  def edit
-  end
-
-  def update
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
